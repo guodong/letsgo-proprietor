ea437c066c0925b031a4b46c88217c8abf39080a
wenke <6wenke@tongji.edu.cn>
2017-07-03 22:35:19 -0400

    add favicon.ico to project

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -53,6 +53,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       filename: config.build.index,
       template: 'index.html',
       inject: true,
+      favicon: 'static/favicon.ico',
       minify: {
         removeComments: true,
         collapseWhitespace: true,
