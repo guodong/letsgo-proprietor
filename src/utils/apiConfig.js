7bffd813f42eca58487b035203cea508abe5dd3f
wenke <6wenke@tongji.edu.cn>
2017-07-25 22:53:08 -0400

    add shop.letsgo.qfplan.com host in nginx config file

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,5 +1,5 @@
-const host = 'http://api.letsgo'
-// const host = 'http://api.letsgo.qfplan.com'
+// const host = 'http://api.letsgo'
+const host = 'http://api.letsgo.qfplan.com'
 
 export const imageHost = host + '/storage/product_images/'
 
