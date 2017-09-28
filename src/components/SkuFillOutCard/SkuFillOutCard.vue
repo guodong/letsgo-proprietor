878bb1bed8ee3690d0b22afd27e90d30bdbbf113
wenke <6wenke@tongji.edu.cn>
2017-09-28 17:51:11 +0800

    deploy

--- src/components/SkuFillOutCard/SkuFillOutCard.vue
+++ src/components/SkuFillOutCard/SkuFillOutCard.vue
@@ -1,9 +1,48 @@
 <template>
-  <box class="form-horizontal" :title="`单品 #${number + 1}: ${productName}${name ? `(${name})` : ''}`" :miniAble="true" :delAble="deleteAble" @deleteBox="deleteSku">
-    <input-group type="text" labelText="名称" :labelSpan="2" :inputSpan="10" placeholder="名称" :value="name" @input="inputName"/>
-    <input-group v-for="(property, index) in properties" :isStatic="true" :labelText="property.name" :value="property.value" :labelSpan="2" :inputSpan="10" :key="index"/>
-    <input-group type="number" labelText="条码" :labelSpan="2" :inputSpan="10" placeholder="条形码" :value="barcode" @input="inputBarcode"/>
-    <input-group type="number" labelText="单价" :labelSpan="2" :inputSpan="10" placeholder="单价" frontAddon="¥" :value="price" @input="inputPrice"/>
+  <box
+    class="form-horizontal"
+    :title="`单品 #${number + 1}: ${productName}${name ? `(${name})` : ''}`"
+    :miniAble="true"
+    :delAble="deleteAble"
+    @deleteBox="deleteSku"
+  >
+    <input-group
+      type="text"
+      labelText="名称"
+      :labelSpan="2"
+      :inputSpan="10"
+      placeholder="名称"
+      :value="name"
+      @input="inputName"
+    />
+    <input-group
+      v-for="(property, index) in properties"
+      :isStatic="true"
+      :labelText="property.name"
+      :value="property.value"
+      :labelSpan="2"
+      :inputSpan="10"
+      :key="index"
+    />
+    <input-group
+      type="number"
+      labelText="条码"
+      :labelSpan="2"
+      :inputSpan="10"
+      placeholder="条形码"
+      :value="barcode"
+      @input="inputBarcode"
+    />
+    <input-group
+      type="number"
+      labelText="单价"
+      :labelSpan="2"
+      :inputSpan="10"
+      placeholder="单价"
+      frontAddon="¥"
+      :value="price"
+      @input="inputPrice"
+    />
     <input-group
       type="text"
       labelText="单位"
@@ -14,13 +53,39 @@
       :value="unit"
       @input="inputUnit"
     />
-    <input-group type="number" labelText="库存" :labelSpan="2" :inputSpan="10" placeholder="库存" :value="stock" @input="inputStock"/>
+    <input-group
+      type="number"
+      labelText="库存"
+      :labelSpan="2"
+      :inputSpan="10"
+      placeholder="库存"
+      :value="stock"
+      @input="inputStock"
+    />
     <div class="form-group">
       <label class="col-sm-2 control-label">销售状态</label>
       <div class="col-sm-10">
         <div class="radio sale-state">
-          <label><input :name="`sale_state_${number}`" type="radio" value="1" :checked="state === 1" @change="checkState">上架</label>
-          <label><input :name="`sale_state_${number}`" type="radio" value="2" :checked="state === 2" @change="checkState">下架</label>
+          <label>
+            <input
+              :name="`sale_state_${number}`"
+              type="radio"
+              value="1"
+              :checked="state === 1"
+              @change="checkState"
+            />
+            上架
+          </label>
+          <label>
+            <input
+              :name="`sale_state_${number}`"
+              type="radio"
+              value="2"
+              :checked="state === 2"
+              @change="checkState"
+            />
+            下架
+          </label>
         </div>
       </div>
     </div>
